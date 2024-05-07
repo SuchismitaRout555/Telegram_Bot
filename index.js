063cd261973d93e526299b04145c19deed8a7044
Suchismita Rout <97534398+SuchismitaRout555@users.noreply.github.com>
2024-05-07 17:32:05 +0530

    added

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const  axios = require('axios');
 const { Telegraf } = require('telegraf');
 
 /**
@@ -9,4 +10,10 @@ const { Telegraf } = require('telegraf');
 
 const bot = new Telegraf('6633796572:AAEtRJXY2oSCgT4zfS5oyIN0MM1SHgoaGZA');
 bot.start((ctx)=> ctx.reply(' Welcome to the GIFT Previous Years Question Bank'));
+
+bot.command('cse' ,  async function(ctx){
+    const response= await axios.get('https://github.com/SuchismitaRout555/GIFT-Question-Bank/blob/main/GIFT-Qbank/img/1stsem-btech-23-24.pdf')
+    console.log(response.data);
+    return ctx.reply(response.data);
+})
 bot.launch();
